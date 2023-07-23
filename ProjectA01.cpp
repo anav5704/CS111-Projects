68c63b500b47cfee495fa54fba0b1e9c64485e2a
Anav Chand <anavkki@gmail.com>
2023-07-24 10:10:00 +1200

    table

---
+++ ProjectA01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+
+    cout << "Beverage" << setw(20) << "Regular" << setw(20) << "Large" << setw(20) << "Extra Large" << setw(20);
+
+    return 0;
+}
