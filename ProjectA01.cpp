cf0a16bea722be2d1c0c76d93f0bd2128ff1e335
Anav Chand <anavkki@gmail.com>
2023-07-24 22:09:51 +1200

    done

--- ProjectA01.cpp
+++ ProjectA01.cpp
@@ -1,23 +1,35 @@
+// Preprocessor Directives
 #include<iostream>
 #include<iomanip>
 using namespace std;
 
 int main() {
 
-    string one = "012345";
-    string two = one.substr(0, 1);
-    bool gay = true;
-    int x = 1000;
+    string berverages[] = {"Long Black", "Flat White", "Cappuccino", "Tea"};
+    string pricesRegular[] = {"$3.50", "$3.80", "$4.00", "$3.00"};
+    string pricesLarge[] = {"$3.70", "$4.20", "$4.30", "$3.20"};
+    string pricesExtraLarge[] = {"$4.00", "$4.50", "$5.00", "$3.50"};
 
-    cout << two << endl;
+    int berveragesLength = sizeof(berverages) / sizeof(berverages[0]);
 
-    cout << sizeof(x) << endl;
+    for (int i = 0; i < berveragesLength; i++){
+        cout << setw(15) << left << berverages[i] << setw(10) << pricesRegular[i] << setw(10) << pricesLarge[i] << setw(10) << pricesExtraLarge[i] << endl;
+    }
 
-    cout << "hey\\\"?" << endl ;
-
-    gay == true ? cout << "youre gay" << endl : cout << "not gay" << endl;
-
-    cout << "Beverage" << setw(20) << "Regular" << setw(20) << "Large" << setw(20) << "Extra Large" << setw(20);
-
+    // cout << " ______________________________________________________ " << endl;
+    // cout << "|                                                      |" << endl; 
+    // cout << "|                Programmers Cafe Menu                 |" << endl; 
+    // cout << "|______________________________________________________|" << endl;
+    // cout << "|Berverage     |Regular     |Large     |Extra Large    |" << endl;
+    // cout << "|______________|____________|__________|_______________|" << endl;
+    // cout << "|Long Black    |$3.50       |$3.70     |$4.00          |" << endl;
+    // cout << "|______________|____________|__________|_______________|" << endl;
+    // cout << "|Flat White    |$3.80       |$4.20     |4.50           |" << endl;
+    // cout << "|______________|____________|__________|_______________|" << endl;
+    // cout << "|Cappuccino    |$4.00       |$4.30     |$5.00          |" << endl;
+    // cout << "|______________|____________|__________|_______________|" << endl;
+    // cout << "|Tea           |$3.00       |$3.20     |$3.50          |" << endl;
+    // cout << "|______________|____________|__________|_______________|" << endl;
+    
     return 0;
-}
+}   
