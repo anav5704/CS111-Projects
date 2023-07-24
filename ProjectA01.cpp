cf67d243981d6d67dbaeabbc8c5e668045b34022
Anav Chand <anavkki@gmail.com>
2023-07-24 15:50:31 +1200

    ?

--- ProjectA01.cpp
+++ ProjectA01.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main() {
 
+    string one = "012345";
+    string two = one.substr(0, 1);
+    bool gay = true;
+    int x = 1000;
+
+    cout << two << endl;
+
+    cout << sizeof(x) << endl;
+
+    cout << "hey\\\"?" << endl ;
+
+    gay == true ? cout << "youre gay" << endl : cout << "not gay" << endl;
+
     cout << "Beverage" << setw(20) << "Regular" << setw(20) << "Large" << setw(20) << "Extra Large" << setw(20);
 
     return 0;
