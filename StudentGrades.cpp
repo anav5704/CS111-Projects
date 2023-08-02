356d5b4534ef92817fe03a485212bffec849435a
Anav Chand <anavkki@gmail.com>
2023-08-02 14:34:55 +1200

    W Dan

--- StudentGrades.cpp
+++ StudentGrades.cpp
@@ -5,12 +5,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "\n\tGrade List for CS111" << endl; cout << "\t--------------------\n\n";
-    cout << "\tGoogle Smith\t\tA" << endl; cout << "\tJohn Pencil\t\tB" << endl
-        << "\tMr. Football\t\tC" << endl; cout << "\tJason Stone\t\tB+"
+    cout << "\n\tGrade List for CS111" << endl; 
+    cout << "\t--------------------\n\n";
+    cout << "\tGoogle Smith\t\tA" << endl; 
+    cout << "\tJohn Pencil\t\tB" << endl 
+        << "\tMr. Football\t\tC" << endl; 
+    cout << "\tJason Stone\t\tB+"
         << endl << "\tStapler Pin\t\tA+" << endl;
-    cout << "\tManila Folder\t\tC+" << endl; cout
-        << "\tKeyboard Hunter\t\tD" << endl; cout << "\tSystem Speaker\t\tE" << endl;
+    cout << "\tManila Folder\t\tC+" << endl; 
+    cout << "\tKeyboard Hunter\t\tD" << endl; 
+    cout << "\tSystem Speaker\t\tE" << endl;
     cout << endl;
     system("PAUSE");
     return 0;
