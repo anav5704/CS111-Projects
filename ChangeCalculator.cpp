67f048ab3f2025a74d6d1e64c1db36680979fd0e
Anav Chand <anavkki@gmail.com>
2023-08-07 09:06:56 +1200

    week 4 labs

---
+++ ChangeCalculator.cpp
@@ -0,0 +1,25 @@
+// Yummy 🍕 & 🧁 Shop 
+// Made by Anav
+// Date: 7/08/32023
+// Purpose: This program calculates the change ee
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+
+    int PIZZAS;
+
+    // enter numebr of pizzas
+
+    // calculate total price
+
+    // enter cash paid ( > total )
+
+    // display change $10, $5, $2, $1, 50c, 20c, 10c, 5
+
+    // $100 $50 or $20 big no no
+
+    system("PAUSE");
+    return 0;
+}
