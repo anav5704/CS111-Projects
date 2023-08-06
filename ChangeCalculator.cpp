32286ca709b309ead59a4560a12809b62c74a51b
Anav Chand <anavkki@gmail.com>
2023-08-07 10:12:52 +1200

    :)

--- ChangeCalculator.cpp
+++ ChangeCalculator.cpp
@@ -1,14 +1,24 @@
 // Yummy 🍕 & 🧁 Shop 
 // Made by Anav
-// Date: 7/08/32023
-// Purpose: This program calculates the change ee
+// Date: 07/08/32023
+// Purpose: This program calculates the change after users input the number of pizzas they want and their payment
+
 #include<iostream>
 #include<iomanip>
 using namespace std;
 
 int main(){
 
-    int PIZZAS;
+    // const int PIZZAS;
+    const string TITLE = "Welcome to the order and checkout for Yummy Pizza & Bakery Shop!";
+    const double PIZZA_PRICE = 10.50;
+
+    // main program
+
+    cout << TITLE << endl;
+    cout << setfill('-') << setw(TITLE.length())  << "" << endl;
+    cout.fill(' ');
+    cout << "We only sell Medium Pizzas for $" << PIZZA_PRICE << " each." << endl;
 
     // enter numebr of pizzas
 
