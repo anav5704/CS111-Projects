80f831ecf9318e58f8a4e968e2f16fd226be73b5
Anav Chand <anavkki@gmail.com>
2023-08-10 09:27:13 +1200

    vector baby!

--- Vectors.cpp
+++ Vectors.cpp
@@ -10,9 +10,14 @@ int main(){
     array.push_back(2);
     array.push_back(3);
 
-    for(int i =0; i < array.size(); i++ ){
+    for(int i = array.size() -1; i > -1; i-- ){
+        cout <<"i: " << i << endl;
         cout << array[i] << endl;
+        array.pop_back();
     }
+
+    // Ternary Expressions are THE shit 🫡
+    array.empty() ? cout << "Yep it's empty"  << endl : cout << "Nah it's got elements" << endl;
 
     system("PAUSE");
     return 0;
