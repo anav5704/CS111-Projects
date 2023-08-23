8cbbe16eadb630eeb8a7738746c94a55a150bbcf
Anav Chand <anavkki@gmail.com>
2023-08-23 15:44:12 +1200

    :)

--- Sentinel.cpp
+++ Sentinel.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main()
 {
     int input_num, positive_numbers = 0, negative_numbers = 0;
+    const int SENTINEL = 0;
+    const int MIN_RANGE = -100;
+    const int MAX_RANGE = 100;
 
     cout << "***********************************************\n";
     cout << "Welcome to the Positive - Negative counter app!\n";
@@ -12,14 +15,14 @@ int main()
     cout << "Press 0 to tally or enter number to add to the counter: " << endl;
     cin >> input_num;
 
-    while(input_num != 0){
-        if(input_num < -100 || input_num > 100){
+    while(input_num != SENTINEL){
+        if(input_num < MIN_RANGE || input_num > MAX_RANGE){
             cout << "Input out of range (-100 - 100), please try again: " << endl;
         }
-        if(input_num > 0){
+        else if(input_num > 0){
             positive_numbers++;
         }
-        if(input_num < 0){
+        else{
             negative_numbers++;
         }
         cin >> input_num;
