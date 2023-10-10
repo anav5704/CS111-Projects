48dac8a3f9055c1796bc31dda014b4f4ffe50ada
Anav Chand <anavkki@gmail.com>
2023-10-10 18:28:21 +1200

    Bro be dangling

---
+++ DanglingElse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string country = "Fiji";
+    bool developingCountry = false;
+
+    if(country == "USA")
+        if(developingCountry) 
+            cout << "Fiji is a developing country" << endl;
+    else 
+        cout << "Fiji is not a developing country" << endl;
+    else 
+        cout << "Not Fiji" << endl;
+
+    return 0;
+    return 0;
+}
