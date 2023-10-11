799aa5c6cb39c159c316a0e88a0c8c3ab97cbe62
Anav Chand <anavkki@gmail.com>
2023-10-11 15:19:55 +1200

    Hi Dan

--- FileWriter.cpp
+++ FileWriter.cpp
@@ -2,38 +2,35 @@
 #include<fstream>
 using namespace std;
 
-double averageArray(double array[], double size); // Prototype
+double averageArray(int array[], int size); // Prototype
  
 int main()
 {
     ofstream writeFile;
     writeFile.open("Data.txt");
 
-    for(int i = 0; i <= 50; i++) if(i % 2 == 0) writeFile << i << " ";
+    for(int i = 0; i <= 100; i++) if(i % 2 == 0) writeFile << i << " ";
 
     writeFile.close();
 
     int size = 0;
-    double length = 0;
-    double data[size]; // only int works 🤷‍♂️
+    int data[size];
 
     ifstream readFile;
     readFile.open("Data.txt");
 
     while(readFile >> data[size++]) // This shi cool af 
-    length = size;
 
     readFile.close();
 
     cout << "Data from array: ";
     for(int i = 0; i < size - 1; i++) cout << data[i] << " ";
-    cout << "Average: " << averageArray(data, length) << endl;
+    cout << "Average: " << averageArray(data, size) << endl;
 
     return 0;
 }
 
-double averageArray(double array[], double size){
+double averageArray(int array[], int size){
     int sum = 0;
     for(int i = 0; i < size - 1; i++) sum += array[i];
-    return sum / size;
-}
+    return (sum * 1.0) / (size * 1.0);}
