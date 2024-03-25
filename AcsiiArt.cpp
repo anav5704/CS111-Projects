e739c85c881dfcbde3651f2469c60eb17fb35b7f
anav5704 <anavkki@gmail.com>
2024-03-25 19:23:25 +1200

    feat: add acsii art program

---
+++ AcsiiArt.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " ><(((('> \n";
+
+    cout << "  /\\   \n";
+    cout << " /  \\  \n";
+    cout << "/____\\ \n";
+    cout << "|    | \n";
+    cout << "|____| \n";
+
+    cout << " /\\_/\\  \n";
+    cout << "( o.o ) \n";
+    cout << " > ^ <  \n";
+
+    cout << "+----------+\n";
+    cout << "|   Anav   | \n";
+    cout << "+----------+ \n";
+
+    cout << "+----------------+----------------------------------+\n";
+    cout << "| Movie          | Reason                           |\n";
+    cout << "+----------------+----------------------------------+\n";
+    cout << "| Titanic        | I have interest in ships         |\n";
+    cout << "+----------------+----------------------------------+\n";
+    cout << "| Jurassic Park  | I like dinosaurs                 |\n";
+    cout << "+----------------+----------------------------------+\n";
+    cout << "| Superman       | I am a DC fan                    |\n";
+    cout << "+----------------+----------------------------------+\n";
+
+    return 0;
+}
