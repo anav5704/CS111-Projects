828cc3c1fe9939706b2c5dab2ddee398a2e8704c
anav5704 <anavkki@gmail.com>
2024-03-26 22:09:43 +1200

    feat: add pattern program

---
+++ Pattern.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    // 3)
+
+    // Using a for loop to iterate from 1 to 20
+    for (int i = 1; i <= 20; ++i)
+    {
+        // Checking if the current number is even
+        if (i % 2 == 0)
+        {
+            // Printing the even number
+            cout << i << " ";
+        }
+    }
+
+    // 4)
+
+    // Constants
+    const int LOWER_LIMIT = 1;
+    const int UPPER_LIMIT = 5;
+
+    // Variables
+    int row_col;
+    int input;
+
+    cout << "Enter the number of rows and columns (1 - 5): ";
+    cin >> row_col;
+
+    // Validate input
+    while (cin.fail() || row_col < LOWER_LIMIT || row_col > UPPER_LIMIT)
+    {
+        // Check if input was a string
+        if (cin.fail())
+        {
+            cin.clear();
+            string dummy;
+            cin >> dummy;
+
+            cout << "Oops! Something went wrong." << endl;
+            cout << "Please enter a number: " << endl;
+        }
+
+        // Check if input was out of range
+        else if (input < LOWER_LIMIT || input > UPPER_LIMIT)
+        {
+            cin.clear();
+            cout << "Oops! Something went wrong." << endl
+                 << "Try entering something between " << LOWER_LIMIT << " and " << UPPER_LIMIT << endl;
+        }
+
+        cin >> row_col;
+    }
+
+    // Print pattern
+    for (int i = 0; i < row_col; i++)
+    {
+        for (int j = 0; j <= i && j < row_col; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
